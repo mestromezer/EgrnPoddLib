24e0fcd759f3aabd7051ff30c60305395cd5bc52
Vimslpb Obsm <global0908@mail.ru>
2023-11-21 10:52:36 +0300

    Refactoring

--- EgrnPoddLib/Data/PoddResponse.cs
+++ EgrnPoddLib/Data/PoddResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EgrnPoddLib.Data
+﻿namespace EgrnPoddLib.Data
 {
     public class PoddResponse
     {
